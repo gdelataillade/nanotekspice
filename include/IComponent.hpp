0726c083bd1cf08ffb516ef08170de434939a09e
Jon Ihlas <jon.ihlas-mouesca@epitech.eu>
2019-01-23 12:23:58 +0100

    Add Icomponent

---
+++ include/IComponent.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_nanotekspice_2018
+** File description:
+** IComponent
+*/
+
+#ifndef ICOMPONENT_HPP_
+#define ICOMPONENT_HPP_
+
+#include <iostream>
+
+namespace nts {
+enum Tristate { UNDEFINED = (-true), TRUE = true, FALSE = false };
+class IComponent {
+   public:
+    virtual ~IComponent() = default;
+
+   public:
+    virtual nts ::Tristate compute(std::size_t pin = 1) = 0;
+    virtual void setLink(std::size_t pin, nts ::IComponent &other,
+                         std::size_t otherPin) = 0;
+    virtual void dump() const = 0;
+};
+}  // namespace nts
+
+#endif /* !ICOMPONENT_HPP_ */
