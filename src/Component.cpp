bcea330bfe791a61ee1f5a2618070ad872c53962
Jon Ihlas <jon.ihlas-mouesca@epitech.eu>
2019-01-24 13:20:48 +0100

    Add logical gate to component

--- src/Component.cpp
+++ src/Component.cpp
@@ -8,16 +8,60 @@
 #include "Component.hpp"
 
 Component::Component(std::string name, std::string type)
-: _name(name), _type(type), nts::IComponent()
-{
+    : _name(name), _type(type), nts::IComponent() {}
+
+nts::Tristate Component::compute(std::size_t pin) {}
+
+void Component::setLink(std::size_t pin, Component &other,
+                        std::size_t otherPin) {
+    _links.insert(pin, otherPin);
+    other._links.insert(otherPin, pin);
 }
 
-nts::Tristate Component::compute(std::size_t pin)
-{
-
+nts::Tristate Component::gateAnd(nts::Tristate &int1, nts::Tristate &int2) {
+    if (!int1 || !int2)
+        return nts::FALSE;
+    else if (int1 && int2)
+        return nts::TRUE;
+    return nts::UNDEFINED;
 }
 
-void Component::setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin)
-{
+nts::Tristate Component::gateOr(nts::Tristate &int1, nts::Tristate &int2) {
+    if (int1 || int2)
+        return nts::TRUE;
+    else if (!int1 && !int2)
+        return nts::FALSE;
+    return nts::UNDEFINED;
+}
 
+nts::Tristate Component::gateNor(nts::Tristate &int1, nts::Tristate &int2) {
+    if (int1 || int2)
+        return nts::FALSE;
+    else if (!int1 && !int2)
+        return nts::TRUE;
+    return nts::UNDEFINED;
+}
+
+nts::Tristate Component::gateXor(nts::Tristate &int1, nts::Tristate &int2) {
+    if ((int1 && int2) || (!int1 && !int2))
+        return nts::FALSE;
+    else if (int1 || int2)
+        return nts::TRUE;
+    return nts::UNDEFINED;
+}
+
+nts::Tristate Component::gateXnor(nts::Tristate &int1, nts::Tristate &int2) {
+    if ((int1 && int2) || (!int1 && !int2))
+        return nts::TRUE;
+    else if (int1 || int2)
+        return nts::FALSE;
+    return nts::UNDEFINED;
+}
+
+nts::Tristate Component::gateNand(nts::Tristate &int1, nts::Tristate &int2) {
+    if (!int1 || !int2)
+        return nts::TRUE;
+    else if (int1 && int2)
+        return nts::FALSE;
+    return nts::UNDEFINED;
 }
