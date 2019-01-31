62ba70555bb6765b9ab7e7df4114ca2064d93580
Gautier de Lataillade <gautier2406@gmail.com>
2019-01-31 13:07:20 +0100

    Add basic prompt

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,40 @@
 
 #include "Parser.hpp"
 
-std::vector<std::pair<std::string, std::size_t>> setInputs(std::vector<std::pair<std::string, std::size_t>> *inputs, int argc, char const *argv[])
+void
+setInputs(std::vector<std::pair<std::string, std::size_t>> *inputs, int argc, char const *argv[])
 {
-    if (argc < 3) {
-        return *inputs;
+    int it = 2;
+    std::string name;
+    int value;
+    int pos;
+    std::pair<std::string, int> pair;
+
+    while (it < argc) {
+        std::cout << argv[it] << ": ";
+        pos = std::string(argv[it]).find("=");
+        name = std::string(argv[it]).substr(0, pos);
+        value = atoi(std::string(argv[it]).substr(pos + 1).c_str());
+        std::cout << name << " ~ " << value << std::endl;
+        pair = std::make_pair(name, value);
+        inputs->push_back(pair);
+        it++;
     }
+}
+
+bool executeCommand(std::string cmd, Circuit **c)
+{
+    if (cmd == "exit") {
+        return false;
+    }
+    if (cmd == "simulate") {
+        std::cout << "simulating..." << std::endl;
+        (*c)->runSimulation();
+    }
+    if (cmd == "display") {
+        std::cout << "displaying..." << std::endl;
+    }
+    return true;
 }
 
 int main(int argc, char const *argv[]) {
@@ -19,7 +48,8 @@ int main(int argc, char const *argv[]) {
         std::cerr << "Don't forget the filepath!" << std::endl;
         return 0;  // TROW EXCEPTION !
     }
-    std::vector<std::pair<std::string, std::size_t>> inputs = setInputs(&inputs, argc, argv);
+    std::vector<std::pair<std::string, std::size_t>> inputs;
+    setInputs(&inputs, argc, argv);
     Circuit *c = new Circuit();
     std::string path(argv[1]);
     Parser p(path, c);
@@ -28,7 +58,10 @@ int main(int argc, char const *argv[]) {
     std::string input;
     while (1) {
         std::cout << "> ";
-        std::cin >> input;
+        std::cin >> input; // read ?
+        if (!executeCommand(input, &c)) {
+            break;
+        }
     }
     return 0;
 }
